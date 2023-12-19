facf7076431d8d58a5fbef1c25897520abb69e96
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-12-19 17:10:58 +0200

    more core settings

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -10,14 +10,32 @@ class Core {
 	protected $params = [];
 
     public function __construct() {
-    	$this->getURL();
+  
+      $url = $this->getURL();
+      if (!empty($url)) {
+      if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+      	$this->currentController = ucwords($url[0]);
+      	unset($url[0]);
+      }
+      }
+
+      //Require controller
+      require_once '../app/controllers/' . $this->currentController . '.php';
+
+      //Instance controller class
+      $this->currentController = new $this->currentController;
+
     }
 
 	public function getURL() {
-     
-     var_dump($_GET['url']);
+      if (isset($_GET['url'])) {
      $url = rtrim($_GET['url'], '/');
-     echo "<pre>";
-     echo $url;
+     $url = filter_var($url, FILTER_SANITIZE_URL);
+     $url = explode('/', $url);
 	}
+	else {
+        $url = [];
+    }
+   return $url;
+}
 }
